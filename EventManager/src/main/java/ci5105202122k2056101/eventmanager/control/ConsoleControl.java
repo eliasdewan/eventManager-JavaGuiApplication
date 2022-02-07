925def30f4c688fb5cb4616848fde5ad943985d2
k2056101 <k2056101@PPRSB2023-D9.kuds.kingston.ac.uk>
2022-02-07 15:15:56 +0000

    Some classes and packages

---
+++ EventManager/src/main/java/ci5105202122k2056101/eventmanager/control/ConsoleControl.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ci5105202122k2056101.eventmanager.control;
+
+/**
+ *
+ * @author k2056101
+ */
+public class ConsoleControl {
+    
+}
